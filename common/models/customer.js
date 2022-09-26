b3410e2826f81898d27e28825ec24481464d6486
bhupender <bhupender.cool98@gmail.com>
2022-09-26 08:56:21 +0530

    added more restpoints

--- common/models/customer.js
+++ common/models/customer.js
@@ -91,6 +91,42 @@ module.exports = function (Customer) {
       console.log("data", data);
       res.send(data);
     });
+    app.get("/cust", async (req, res) => {
+      // let data = Customer.app.models.find();
+      // console.log("data", data);
+      let connector = Customer.app.datasources.mysqldata.connector;
+      let cust = Customer.find();
+      //console.log("connector", connector);
+      let sql = "select * from customers";
+      console.log("cust", cust);
+      connector.execute(sql, null, (err, data) => {
+        if (err) console.log(err);
+        console.log("data", data);
+        res.send(data);
+      });
+
+      // let sql = "select * from customers";
+      // let data = await connector.find();
+      // console.log("data", data);
+    });
+    app.get("/cust", async (req, res) => {
+      // let data = Customer.app.models.find();
+      // console.log("data", data);
+      let connector = Customer.app.datasources.mysqldata.connector;
+      let cust = Customer.find();
+      //console.log("connector", connector);
+      let sql = "select * from customers";
+      console.log("cust", cust);
+      connector.execute(sql, null, (err, data) => {
+        if (err) console.log(err);
+        console.log("data", data);
+        res.send(data);
+      });
+
+      // let sql = "select * from customers";
+      // let data = await connector.find();
+      // console.log("data", data);
+    });
 
     // let sql = "select * from customers";
     // let data = await connector.find();
