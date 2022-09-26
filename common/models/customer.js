b4217557b9206fd9cf43ebd0ed4f07b0b9cd3bb4
bhupender <bhupender.cool98@gmail.com>
2022-09-26 08:53:20 +0530

    added rest point

--- common/models/customer.js
+++ common/models/customer.js
@@ -42,4 +42,58 @@ module.exports = function (Customer) {
     // let data = await connector.find();
     // console.log("data", data);
   });
+  app.get("/cust", async (req, res) => {
+    // let data = Customer.app.models.find();
+    // console.log("data", data);
+    let connector = Customer.app.datasources.mysqldata.connector;
+    let cust = Customer.find();
+    //console.log("connector", connector);
+    let sql = "select * from customers";
+    console.log("cust", cust);
+    connector.execute(sql, null, (err, data) => {
+      if (err) console.log(err);
+      console.log("data", data);
+      res.send(data);
+    });
+
+    // let sql = "select * from customers";
+    // let data = await connector.find();
+    // console.log("data", data);
+  });
+  app.get("/cust", async (req, res) => {
+    // let data = Customer.app.models.find();
+    // console.log("data", data);
+    let connector = Customer.app.datasources.mysqldata.connector;
+    let cust = Customer.find();
+    //console.log("connector", connector);
+    let sql = "select * from customers";
+    console.log("cust", cust);
+    connector.execute(sql, null, (err, data) => {
+      if (err) console.log(err);
+      console.log("data", data);
+      res.send(data);
+    });
+
+    // let sql = "select * from customers";
+    // let data = await connector.find();
+    // console.log("data", data);
+  });
+  app.get("/cust", async (req, res) => {
+    // let data = Customer.app.models.find();
+    // console.log("data", data);
+    let connector = Customer.app.datasources.mysqldata.connector;
+    let cust = Customer.find();
+    //console.log("connector", connector);
+    let sql = "select * from customers";
+    console.log("cust", cust);
+    connector.execute(sql, null, (err, data) => {
+      if (err) console.log(err);
+      console.log("data", data);
+      res.send(data);
+    });
+
+    // let sql = "select * from customers";
+    // let data = await connector.find();
+    // console.log("data", data);
+  });
 };
